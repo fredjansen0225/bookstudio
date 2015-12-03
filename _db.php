1006c86f80a16b571e3595dfdc6693d638829e17
Fredrick Jansen <fred.jansen0225@hotmail.com>
2015-12-03 09:29:24 +0100

    create dbMysql object

--- _db.php
+++ _db.php
@@ -46,17 +46,17 @@ if (!$db_exists) {
 }
 
 
-// $dbMysql = new MysqliDb (Array (
-//                 'host' => 'localhost',
-//                 'username' => 'root', 
-//                 'password' => '',
-//                 'db'=> '3rdStreetAdr',
-//                 'port' => 3306,
-//                 'prefix' => 'my_',
-//                 'charset' => 'utf8'));
+$dbMysql = new MysqliDb (Array (
+                'host' => 'localhost',
+                'username' => 'root', 
+                'password' => '',
+                'db'=> '3rdStreetAdr',
+                'port' => 3306,
+                'prefix' => 'my_',
+                'charset' => 'utf8'));
 
-// $doctor_tb_creation = "CREATE TABLE `3rdStreetAdr`.`doctor` ( `doctor_id` INT NOT NULL AUTO_INCREMENT , `doctor_name` VARCHAR(100) NOT NULL , PRIMARY KEY (`doctor_id`)) ENGINE = InnoDB;";
-// $appointment_tb_creation = "CREATE TABLE `3rdStreetAdr`.`appointment` ( `id` INT NOT NULL AUTO_INCREMENT , `start` DATETIME NOT NULL , `end` DATETIME NOT NULL , `patient_name` VARCHAR(100) NOT NULL , `status` VARCHAR(100) NOT NULL DEFAULT 'free' , `patient_session` VARCHAR(100) NOT NULL , `doctor_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+$doctor_tb_creation = "CREATE TABLE `3rdStreetAdr`.`doctor` ( `doctor_id` INT NOT NULL AUTO_INCREMENT , `doctor_name` VARCHAR(100) NOT NULL , PRIMARY KEY (`doctor_id`)) ENGINE = InnoDB;";
+$appointment_tb_creation = "CREATE TABLE `3rdStreetAdr`.`appointment` ( `id` INT NOT NULL AUTO_INCREMENT , `start` DATETIME NOT NULL , `end` DATETIME NOT NULL , `patient_name` VARCHAR(100) NOT NULL , `status` VARCHAR(100) NOT NULL DEFAULT 'free' , `patient_session` VARCHAR(100) NOT NULL , `doctor_id` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
 
 
 
