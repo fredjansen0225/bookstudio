a700e58b30ba9f45c58ecde637e65d2f6bbe7786
Fredrick Jansen <fred.jansen0225@hotmail.com>
2015-12-04 17:26:48 +0100

    fix title

--- _header.php
+++ _header.php
@@ -1,8 +1,8 @@
         <div id="header">
             <div class="bg-help">
                 <div class="inBox">
-                    <h1 id="logo"><a href='http://code.daypilot.org/79305/angularjs-doctor-appointment-scheduling-tutorial'>AngularJS Doctor Appointment Scheduling Tutorial</a></h1>
-                    <p id="claim"><a href="http://javascript.daypilot.org/">DayPilot for JavaScript</a> - AJAX Calendar/Scheduling Widgets for JavaScript/HTML5/jQuery/AngularJS</p>
+                    <h1 id="logo"><a href='#'>3rdStreetAdr Mixing Studio</a></h1>
+                    <p id="claim">Studio Booking Calendar</p>
                     <hr class="hidden" />
                 </div>
             </div>
